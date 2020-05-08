9f96d209e6caedb65a4b90cd50e0dae6af1329ed
Raylan Sales <Raylanwork@gmail.com>
2020-05-08 00:47:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 //Compile com a flag -lcurses
 #include<ncurses.h>
 #include<ctype.h>
+#include<stdlib.h>
+
+/* macro definition for console clearing */
+#ifdef _WIN32
+# define CLEAR_SCREEN system ("cls")
+#else
+# define CLEAR_SCREEN puts("\x1b[H\x1b[2J")
+#endif
 
 int found[4096], i;
 char word[] = "GERALT", uppercase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -10,19 +18,22 @@ typedef enum { STARTING, RUNNING, WAITING, VICTORY, GAME_OVER } State; // estado
 #include "update.h"
 #include "init.h"
 #include "close.h"
+#include "menu.h"
 
 int main(){
     int tries = 7;
     State state = STARTING;
 
-    init();
+    if(menu() == 1){
+        init();
 
-    while(state != VICTORY && state != GAME_OVER){
-        state = update(state, &tries);
-        print(tries, state);
+        while(state != VICTORY && state != GAME_OVER){
+            state = update(state, &tries);
+            print(tries, state);
+        }
+
+        close();
     }
-
-    close();
-    
+        
     return 0;
 }
