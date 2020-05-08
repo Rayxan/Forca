ca34eaabc1bfb146f39da05820b7651a5e341d4b
Raylan Sales <Raylanwork@gmail.com>
2020-05-08 00:47:41 -0300

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,13 @@
+int menu(){
+    int op;
+    printf("\tMENU\n");
+    printf("1 - Jogar\n");
+    printf("2 - Sair\n");
+    printf("Digite uma das opcoes: ");
+
+    scanf("%d", &op);
+
+    system("clear");
+
+    return (op == 1) ? 1 : 2;
+}
